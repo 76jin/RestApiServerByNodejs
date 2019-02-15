2cbc15f2b213064c0a044f264fbb909a4c8a1fd1
LeeKangJin <ranian129@gmail.com>
2019-02-15 18:40:43 +0900

    add logger(console, file).

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
-var logger = require('morgan');
+var morgan = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,11 +12,18 @@ var processRouter = require('./routes/process');
 
 var app = express();
 
+var winston = require('./winston');
+var logger = winston;
+
+app.use(morgan('combined', {
+  stream: winston.stream
+}));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+//app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
@@ -44,6 +51,9 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // add this line to include winston logging
+  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
 
   // render the error page
   res.status(err.status || 500);
---
+++ winston.js
@@ -0,0 +1,48 @@
+var appRoot = require('app-root-path');
+var winston = require('winston');
+
+// define the custom settings for each transport (file, console)
+var options = {
+    file: {
+        level: 'info',
+        filename: `${appRoot}/logs/app.log`,
+        handleExceptions: true,
+        json: false,
+        format: winston.format.simple(),
+        maxsize: 5242880, // 5MB
+        maxFiles: 5,
+        colorize: true,
+    },
+    console: {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        format: winston.format.combine(
+            winston.format.timestamp({
+                format: 'YYYY-MM-DD HH:mm:ss'
+            }),
+            winston.format.colorize(),
+            winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)
+        ),
+        colorize: true,
+    },
+};
+
+// instantiate a new Winston Logger with the settings defined above
+var logger = winston.createLogger({
+    transports: [
+        new winston.transports.File(options.file),
+        new winston.transports.Console(options.console)
+    ],
+    exitOnError: false, // do not exit on handled exceptions
+});
+
+// create a stream object with a 'write' function that will be used by `morgan`
+logger.stream = {
+    write: function (message, encoding) {
+        // use the 'info' log level so the output will be picked up by both transports (file and console)
+        logger.info(message);
+    },
+};
+
+module.exports = logger;
